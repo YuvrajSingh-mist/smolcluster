10fe3e658e3cf285e8862e2fec48a36d87640edf
YuvrajSingh-mist <yuvraj.mist@gmail.com>
2026-01-05 23:14:52 +0530

    Refactor worker script to use host_ip configuration for server connection and remove server_connect_ip

--- src/smolcluster/SimpleAllReduce/worker.py
+++ src/smolcluster/SimpleAllReduce/worker.py
@@ -46,7 +46,7 @@ else:
 local_rank = int(WORKER_RANK)
 
 # Workers connect to the server using the IP specified for this worker's hostname
-HOST_IP = cluster_config["server_connect_ip"][HOSTNAME]
+HOST_IP = cluster_config["host_ip"][HOSTNAME]
 batch_size = nn_config["batch_size"]
 num_epochs = nn_config["num_epochs"]
 eval_steps = nn_config["eval_steps"]
