67be05ffc1b30c5088009d159ccd451ad6b36ead
YuvrajSingh-mist <yuvraj.mist@gmail.com>
2026-01-08 21:18:11 +0530

    [fix] fixed the total steps to account for the entirety of the dataset

--- src/smolcluster/ElasticDistributedTraining/server.py
+++ src/smolcluster/ElasticDistributedTraining/server.py
@@ -342,7 +342,7 @@ def main():
     
     train_iter = iter(train_loader)
 
-    total_steps = num_epochs * len(train_loader)
+    total_steps = num_epochs * len(train_loader) * NUM_WORKERS
    
     for step in range(total_steps):
         model.train()
