a8ff6a9e874ee473bebf47dd49944efc550c72ba
YuvrajSingh-mist <yuvraj.mist@gmail.com>
2026-01-12 21:31:04 +0530

    Update cluster configuration to activate win as a worker and adjust SimpleNN model by commenting out unused layers and simplifying forward pass

--- src/smolcluster/models/SimpleNN.py
+++ src/smolcluster/models/SimpleNN.py
@@ -9,13 +9,13 @@ class SimpleMNISTModel(nn.Module):
         self.fc1 = nn.Linear(input_dim, 256)
         self.relu1 = nn.ReLU()
         self.norm = nn.LayerNorm(256)
-        self.fc2 = nn.Linear(256, 4 * hidden)
+        # self.fc2 = nn.Linear(256, 4 * hidden)
         self.relu2 = nn.ReLU()
-        self.norm2 = nn.LayerNorm(4 * hidden)
+        # self.norm2 = nn.LayerNorm(4 * hidden)
         self.fc3 = nn.Linear(4 * hidden, out)
 
     def forward(self, x):
-        x = self.relu1(self.norm((self.fc1(x))))
-        x = self.relu2(self.norm2(self.fc2(x)))
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
         x = self.fc3(x)
         return x
