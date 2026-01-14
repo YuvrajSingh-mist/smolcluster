Fix logging gradient message and allow non-strict loading of model state_dict in worker

src/smolcluster/algorithms/EDP/server.py
@@ -411,7 +411,7 @@ def run_edp_server(
             logger.info("Tracking gradients in wandb...")
             for name, param in model.named_parameters():
                 if param.grad is not None:
-                    logger.info(f"Logging gradients for layer: {name}")
+                    
                     grad_norm = torch.norm(param.grad.detach(), 2).item()
                     wandb.log(
                         {